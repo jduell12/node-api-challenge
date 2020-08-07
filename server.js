ff24bde64197924477b5447b125634097767d17a
Jessica <duellje@oregonstate.edu>
2020-08-07 12:46:07 -0400

    added cors to project for stretch

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const server = express();
 
 const projectRouter = require("./routers/projectRouter");
 const actionRouter = require("./routers/actionRouter");
 
 server.use(express.json());
+server.use(cors());
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "It's working!" });
